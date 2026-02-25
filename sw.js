5c67abcce05a6ad233bb73d49b91b9868f45c19d
0pal33 <161562932+0pal33@users.noreply.github.com>
2026-02-26 02:51:01 +0700

    Update sw.js bug fix

--- sw.js
+++ sw.js
@@ -1,3 +1,5 @@
-self.addEventListener('install', function(e) {
- console.log('Service Worker Ready');
+self.addEventListener('install', e => {
+  self.skipWaiting();
 });
+
+self.addEventListener('fetch', e => {});
